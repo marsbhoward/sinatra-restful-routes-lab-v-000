a9fa419ca45a89c43007cf53e8a4f8fd05de2659
Mars Howard <marsbhoward@gmail.com>
2017-12-01 18:07:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,35 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
-  # register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
-
-  get '/recipes/new' do #loads new form
-    erb :new
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
   end
-
-  get '/recipes' do #loads index page
+  get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do  #loads show page
-    @recipe = Recipe.find_by_id(params[:id])
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.new(params)
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
     erb :show
   end
 
-  get '/recipes/:id/edit' do #loads edit form
-    @recipe = Recipe.find_by_id(params[:id])
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
-  patch '/recipes/:id' do  #updates a recipe
-    @recipe = Recipe.find_by_id(params[:id])
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
@@ -32,16 +37,9 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  post '/recipes' do  #creates a recipe
-    @recipe = Recipe.create(params)
-    redirect to "/recipes/#{@recipe.id}"
-  end
-
-  delete '/recipes/:id/delete' do #delete action
-    @recipe = Recipe.find_by_id(params[:id])
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
     @recipe.delete
-    redirect to '/recipes'
+    redirect to '/'
   end
-
-
 end
